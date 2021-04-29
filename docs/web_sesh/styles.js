56df09f3bd50e19283ab69a6a9c7b8b5372aa462
Anthony Wlodarczyk <amw6765@psu.edu>
2021-04-29 19:35:44 -0400

    Cytopgraph Smile

---
+++ docs/web_sesh/styles.js
@@ -0,0 +1,200 @@
+var styles = [ {
+  "format_version" : "1.0",
+  "generated_by" : "cytoscape-3.8.2",
+  "target_cytoscapejs_version" : "~2.1",
+  "title" : "default",
+  "style" : [ {
+    "selector" : "node",
+    "css" : {
+      "height" : 35.0,
+      "border-color" : "rgb(204,204,204)",
+      "shape" : "diamond",
+      "font-size" : 20,
+      "text-valign" : "center",
+      "text-halign" : "center",
+      "text-opacity" : 1.0,
+      "color" : "rgb(0,0,0)",
+      "border-opacity" : 1.0,
+      "font-family" : "SansSerif",
+      "font-weight" : "normal",
+      "border-width" : 0.0,
+      "background-color" : "rgb(137,208,245)",
+      "background-opacity" : 1.0,
+      "width" : 35.0,
+      "content" : "data(name)"
+    }
+  }, {
+    "selector" : "node[name = 'ALEXIOS']",
+    "css" : {
+      "shape" : "triangle"
+    }
+  }, {
+    "selector" : "node:selected",
+    "css" : {
+      "background-color" : "rgb(255,255,0)"
+    }
+  }, {
+    "selector" : "edge",
+    "css" : {
+      "line-color" : "rgb(132,132,132)",
+      "font-family" : "Dialog",
+      "font-weight" : "normal",
+      "line-style" : "solid",
+      "target-arrow-color" : "rgb(0,0,0)",
+      "font-size" : 10,
+      "target-arrow-shape" : "none",
+      "width" : 2.0,
+      "source-arrow-color" : "rgb(0,0,0)",
+      "color" : "rgb(0,0,0)",
+      "content" : "",
+      "opacity" : 1.0,
+      "source-arrow-shape" : "none",
+      "text-opacity" : 1.0
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'The Cult and the Archon']",
+    "css" : {
+      "line-color" : "rgb(102,255,102)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'The Last Riddle']",
+    "css" : {
+      "line-color" : "rgb(102,255,102)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Uprising']",
+    "css" : {
+      "line-color" : "rgb(102,255,102)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'A Debt to Pay']",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Public Opinion']",
+    "css" : {
+      "line-color" : "rgb(0,0,255)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Rising from the Ashes']",
+    "css" : {
+      "line-color" : "rgb(0,0,255)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Another Day, Another Drachma']",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Homecoming']",
+    "css" : {
+      "line-color" : "rgb(0,0,255)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Land of the Lawless']",
+    "css" : {
+      "line-color" : "rgb(0,0,255)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'The Gates of Atlantis']",
+    "css" : {
+      "line-color" : "rgb(102,255,102)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Shadows of Serpents']",
+    "css" : {
+      "line-color" : "rgb(0,0,255)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'The Reunion']",
+    "css" : {
+      "line-color" : "rgb(102,255,102)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'A Place of Twists and Turns']",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'The Cultist King']",
+    "css" : {
+      "line-color" : "rgb(102,255,102)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'A Prescription for Discovery']",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'An Old Enemy and a New Ally']",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Home Is Where My Mother Is']",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Beware the Snakes']",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'The Wolf Hunt']",
+    "css" : {
+      "line-color" : "rgb(102,255,102)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Paradise Lost']",
+    "css" : {
+      "line-color" : "rgb(0,0,255)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'The Wolf and Snake']",
+    "css" : {
+      "line-color" : "rgb(102,255,102)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'The Olympian']",
+    "css" : {
+      "line-color" : "rgb(102,255,102)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'The Road to the Symposium']",
+    "css" : {
+      "line-color" : "rgb(102,255,102)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Battle of Amphipolis']",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'From the Shadows']",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Paint It Red']",
+    "css" : {
+      "line-color" : "rgb(0,0,255)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Pirate Hospitality']",
+    "css" : {
+      "line-color" : "rgb(0,0,255)"
+    }
+  }, {
+    "selector" : "edge[shared_interaction = 'Of Visions and Visionaries']",
+    "css" : {
+      "line-color" : "rgb(0,0,255)"
+    }
+  }, {
+    "selector" : "edge:selected",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  } ]
+} ]
